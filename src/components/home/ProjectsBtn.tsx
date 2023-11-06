17ac7cd3f9bd2a0d285db2bbd0afc1d21e8a9db9
Marcello Nazar <marcellonazarb@hotmail.com>
2023-11-06 15:17:50 -0300

    first commit

--- src/components/home/ProjectsBtn.tsx
+++ src/components/home/ProjectsBtn.tsx
@@ -9,7 +9,7 @@ const ProjectsBtn = () => {
       <Link
         prefetch={false}
         href={"/projects"}
-        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+        className="relative w-[185px] h-[185px] flex justify-center items-center group"
       >
         <Image
           src="/rounded-text.png"
